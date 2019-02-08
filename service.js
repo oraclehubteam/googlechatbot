9e340015c8c693fe35d87fa62bd617e3e0d23af8
gustavogaspar <gaspar.gualves@gmail.com>
2019-02-08 16:53:28 -0200

    general

--- service.js
+++ service.js
@@ -16,8 +16,8 @@ module.exports = (app) => {
   // add webhook integration to Oracle Cloud
   const webhook = new WebhookClient({
     channel: {
-      url: process.env.BOT_WEBHOOK_URL,
-      secret: process.env.BOT_WEBHOOK_SECRET,
+      url: /*<BOT_WEBHOOK_URL>*/,
+      secret: /*<BOT_WEBHOOK_URL>*/,
     }
   });
   // Add webhook event handlers (optional)
