d92f84b293d93779a391eba608276707e0fdf4da
gustavogaspar <gaspar.gualves@gmail.com>
2019-03-01 11:03:45 -0300

    webhook

--- service.js
+++ service.js
@@ -12,8 +12,8 @@ module.exports = (app) => {
 
   const webhook = new WebhookClient({
     channel: {
-      url: /*<BOT_WEBHOOK_URL>*/,
-      secret: /*<BOT_WEBHOOK_URL>*/,
+      url: 'https://botv2iad1I0100H203896bots-mpaasocimt.botmxp.ocp.oraclecloud.com:443/connectors/v1/tenants/idcs-100b89d671b54afca3069fe360e4bad4/listeners/webhook/channels/d0743e8a-c3f4-4bc8-8b80-bd80794c7a58',
+      secret: 'E2Xqx4YQtfFbiEg0NTVN2u3gImC5egPQ',
     }
   });
 
